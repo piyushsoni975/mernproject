4beff911d794f9ae1d6967ffda3f927fb8afde77
piyushsoni975 <piyushkrsoni975@gmail.com>
2024-08-06 01:56:07 +0530

    first commit

---
+++ controller/user.controller.js
@@ -0,0 +1,46 @@
+import User from '../model/user.model.js';
+import bcryptjs from 'bcryptjs'; 
+
+export const signup = async (req,res)=>{
+    try {
+        const {name,email,password} = req.body;
+        const user =await User.findOne({email});
+        if(user){
+            return res.status(400).json({message: "User already exists"});
+        }
+        const hashpassword = await bcryptjs.hash(password,10);
+        const createdUser = new User({
+            name: name,
+            email: email,
+            password: hashpassword,
+        });
+        await createdUser.save();
+        res.status(201).json({message: "User created successfully",user:{
+            _id: createdUser._id,
+            name: createdUser.name,
+            email: createdUser.email,
+        }});
+    } catch (error) {
+        console.log("Error: "+ error.message);
+        res.status(500).json({message: "Internal server error"});
+    }
+};
+
+export const login = async (req,res)=>{
+    try {
+        const {email,password} = req.body;
+        const user = await User.findOne({email});
+        const isMatch = await bcryptjs.compare(password,user.password);
+        if(!user || !isMatch){
+            return res.status(400).json({message: "Invalid credentials"});
+        }
+        res.status(200).json({message: "Login Successful",user:{
+            _id:user._id,
+            name: user.name,
+            email: user.email
+        }});
+    } catch (error) {
+        console.log("Error: "+ error.message);
+        res.status(500).json({message: "Internal server error"});
+    }
+}
---
+++ index.js
@@ -0,0 +1,37 @@
+import express from 'express';
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+import cors from 'cors';
+
+import bookRoute from "./route/book.route.js";
+import userRoute from "./route/user.route.js";
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+dotenv.config();
+
+const PORT=process.env.PORT || 4000;
+const URI=process.env.MongoDBURI;
+
+// Connect to MongoDB
+try {
+    mongoose.connect(URI,{
+        useNewUrlParser: true,
+        UseUnifiedTopology: true
+    });
+    console.log("Connected to mongoDB Database");
+} catch (error) {
+    console.log("Error: ",error);
+}
+
+
+// Defining Routes
+app.use("/book",bookRoute);
+app.use("/user",userRoute);
+
+app.listen(PORT, () => {
+  console.log(`Example app listening on port ${PORT}`)
+})
